3202b66256493119e0a82ec1d291d4e9afb1f605
yugkashyap30-cpu <yugkashyap30@gmail.com>
2026-06-17 15:36:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+let time = 18;
 
+const timer = document.getElementById("timer");
+
+setInterval(() => {
+
+if(time > 0){
+
+time--;
+
+let seconds = time.toString().padStart(2,"0");
+
+timer.innerText = `00:${seconds}`;
+
+}
+
+},1000);
